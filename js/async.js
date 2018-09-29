d4e5860a09478c0fd287a2e9cbaffccac9c58efc
p1608040 <thomas.peyrot@etu.univ-lyon1.fr>
2018-09-29 18:25:10 +0200

    Initialisation de la promesse et création de la variable 'globale' dans js/async.js

--- js/async.js
+++ js/async.js
@@ -1,10 +1,22 @@
 createGeoData()
-.then(generateHTML)
+/*.then(generateHTML)
 .then(generateMap)
 .then(movePOV)
 .then(displayPath)
 .then(generateFilesTab)
 .then(generateGraph)
 .then(generatePoints)
-.then(setListeners)
+.then(setListeners)*/
+.then(console.log)
 .catch(console.log);
+
+function createGeoData() {
+	return new Promise((resolve, reject) => {
+		let geoData = {
+			map: undefined,
+			paths: []
+		};
+		resolve(geoData);
+		reject("Error when initializing the global variable");
+	});
+}
