952d89c911370f5756d0439fe3a952213b4420ac
p1608040 <thomas.peyrot@etu.univ-lyon1.fr>
2018-11-04 16:56:32 +0100

    Lorsque l'on clique droit sur la carte ou les boutons, le menu par défaut ne s'affiche plus

--- js/main.js
+++ js/main.js
@@ -340,6 +340,10 @@ function deleteTrace(geoData, id) {
 
 
 function setListeners(geoData) {
+	// General
+	geoData.map.addEventListener("contextmenu", e => e.preventDefault());
+	document.querySelectorAll("#features button").forEach(button => button.addEventListener("contextmenu", e => e.preventDefault()));
+
 	// Files import
     document.getElementById("importButton").addEventListener("click", () => upload(geoData));
 	document.getElementById("hiddenButton").addEventListener("change", () => hiddenUpload(geoData));
