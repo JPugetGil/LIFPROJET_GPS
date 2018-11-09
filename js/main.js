03de046281ce662e655d32b1f35971cd86ac2e11
p1608040 <thomas.peyrot@etu.univ-lyon1.fr>
2018-11-09 12:38:44 +0100

    Ajout d'un fond de carte "OpenRiverBoatMap"

--- js/main.js
+++ js/main.js
@@ -88,7 +88,8 @@ function generateTiles(geoData) {
 		id: 'mapbox.streets'
 	}).addTo(geoData.map), "Epurée");
 	geoData.layersControl.addBaseLayer(L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar'}), "Détaillée");
-
+	geoData.layersControl.addBaseLayer(L.tileLayer(' http://{s}.tile.openstreetmap.fr/openriverboatmap/{z}/{x}/{y}.png'), "OpenRiverboatMap");
+	
 	return geoData;
 }
 
